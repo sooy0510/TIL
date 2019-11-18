ee4b665f8ad22e38918956950dce1a8ef8d9eecf
sooy0510 <qkdqnwpwp@naver.com>
2019-11-18 13:10:41 +0900

    191118 | MovieRating-ImageField로 변경

--- 05_Django/connected_PJT2/movies/models.py
+++ 05_Django/connected_PJT2/movies/models.py
@@ -1,18 +1,19 @@
 from django.db import models
-from imagekit.models import ProcessedImageField
-from imagekit.processors import Thumbnail
+# from imagekit.models import ProcessedImageField
+# from imagekit.processors import Thumbnail
 
 # Create your models here.
 class Movie(models.Model):
   title = models.CharField(max_length=50)
   description = models.TextField()
-  poster = ProcessedImageField(
-    processors=[Thumbnail(200,300)],  # 처리할 작업
-    format='JPEG',                    # 이미지 포맷
-    options={'quality':90},           # 각종 추가 옵션
-    upload_to='movies/images',      # 저장 위치   
-    # 실제경로 -> MEDIA_ROOT/movies/images
-  ) 
+  # poster = ProcessedImageField(
+  #   processors=[Thumbnail(200,300)],  # 처리할 작업
+  #   format='JPEG',                    # 이미지 포맷
+  #   options={'quality':90},           # 각종 추가 옵션
+  #   upload_to='movies/images',      # 저장 위치   
+  #   # 실제경로 -> MEDIA_ROOT/movies/images
+  # ) 
+  poster = models.ImageField(blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
   user = models.IntegerField(default=1)
@@ -20,7 +21,6 @@ class Movie(models.Model):
   def __str__(self):
       return f'[{self.pk}]:{self.title}'
   
- 
 
 class Rating(models.Model):
   movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
@@ -29,6 +29,6 @@ class Rating(models.Model):
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
   user = models.IntegerField(default=1)
-
+  
   def __str__(self):
     return self.content
